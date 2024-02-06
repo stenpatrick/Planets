cf53e5b2196b9fe961655223c3339f309bc305bd
Opilane TTHK <opilane@tthk.ee>
2024-02-06 13:15:43 +0200

    2

---
+++ Planets/Services/PlanetServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+
+namespace Services
+{
+    internal static class PlanetServices
+    {
+        private static List<Planet> planets = new()
+        {
+            new()
+            {
+
+            }
+        };
+    }
+}
